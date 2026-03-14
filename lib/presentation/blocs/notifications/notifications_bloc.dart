c1ec9826624cfdbe364302543018e64521af01f9
Gabriel <gabrielbaez023@gmail.com>
2026-03-14 10:41:44 -0300

    Escuchar mensajes Push

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -13,9 +13,11 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   NotificationsBloc() : super(NotificationsState()) {
      on<NotificationsStatusChanged>(_notificationStatusChanged);
+    //Verificar estado de las aplicaciones
+    _initialStatusCheck();
 
-     _initialStatusCheck();
-
+    // Listener para notificaaciones en foreground
+    _onForegroundMessage();
   }
 
   static Future<void> initializeFCM() async{
@@ -30,22 +32,36 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
         status: event.status
       )
     );
+    _getFCMToken();
   }
 
   void _initialStatusCheck() async {
     //con este setting yo puedo saber el estado actual
     final settings = await messaging.getNotificationSettings();
     add(NotificationsStatusChanged(settings.authorizationStatus));
-    _gitFCMToken();
+    _getFCMToken();
 
   }
 
-  void _gitFCMToken() async {
+  void _getFCMToken() async {
     final settings = await messaging.getNotificationSettings();
     if( state.status != AuthorizationStatus.authorized) return;
 
     final token = await messaging.getToken();
     print(token);
+  }
+
+  void _handleRemoteMessage( RemoteMessage message){
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification == null) return;
+      print('Message also contained a notification: ${message.notification}');
+
+  }
+
+  void _onForegroundMessage(){
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
   }
 
   void requestPermission() async {
@@ -60,7 +76,7 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     );
 
     add(NotificationsStatusChanged(settings.authorizationStatus));
-    _gitFCMToken();
+    _getFCMToken();
   }
 
 }
