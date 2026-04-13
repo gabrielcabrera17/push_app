e49ebeb1d795921080cdd5482c00eb5aaafa5c7e
Gabriel <gabrielbaez023@gmail.com>
2026-04-13 20:39:43 -0300

    LocalNotifications - Android

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_app/config/loca_notifications/local_notifications.dart';
 import 'package:push_app/domain/entities/push_message.dart';
 import 'package:push_app/firebase_options.dart';
 
@@ -112,6 +113,8 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       provisional: false,
       sound: true,
     );
+    //Solicitar permiso a las local Notifications
+    await requestPermissionLocalNotifications();
 
     add(NotificationsStatusChanged(settings.authorizationStatus));
     _getFCMToken();
